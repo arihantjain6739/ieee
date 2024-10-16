77c6413f6730c2ee818348aef3a0c4e1546ed433
Arihant <arihantjain6739@gmail.com>
2024-10-16 15:12:22 +0530

    Initial commit

---
+++ ieee/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+return (
+    <>
+        <div class="root">
+            <div>
+                <p className='text-white text-4xl mx-10 my-10 font-bold leading-relaxed'>
+                    Some Pieces Of Our
+                    <br />
+                    Work
+                </p>
+                <p className='text-gray-400 font-bold mx-10 leading-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    <br /> Facilis necessitatibus eum nam soluta hic cupiditate <br />amet deleniti laborum laboriosam. Aut!
+                    </p>
+            </div>
+            <button className='text-yellow-400 text-xs font-bold border border-yellow-400 px-5 py-3 my-10 mx-10 rounded-[20px]'>SHOW MORE</button>
+        </div>
+    </>
+)
+}
+
+export default App
