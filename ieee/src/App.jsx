dbcec89860f6dd3c6517d6b0d89be6a0a7f7e7c5
Arihant <arihantjain6739@gmail.com>
2024-10-16 16:54:17 +0530

    inital commit

--- ieee/src/App.jsx
+++ ieee/src/App.jsx
@@ -10,16 +10,26 @@ return (
     <>
         <div class="root">
             <div>
+            <div>
                 <p className='text-white text-4xl mx-10 my-10 font-bold leading-relaxed'>
                     Some Pieces Of Our
                     <br />
                     Work
                 </p>
+                
                 <p className='text-gray-400 font-bold mx-10 leading-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     <br /> Facilis necessitatibus eum nam soluta hic cupiditate <br />amet deleniti laborum laboriosam. Aut!
                     </p>
             </div>
+            {/* commet */}
             <button className='text-yellow-400 text-xs font-bold border border-yellow-400 px-5 py-3 my-10 mx-10 rounded-[20px]'>SHOW MORE</button>
+           
+            </div>
+               <div className="box">
+                <div className="box1"></div>
+                <div className="box2"></div>
+               </div>
+
         </div>
     </>
 )
